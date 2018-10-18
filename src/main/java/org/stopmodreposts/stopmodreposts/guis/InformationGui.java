29e48e80b983a0e191def3b3578aaddc83eb0e8d
JTK222 <lukasporwit3@googlemail.com>
2018-10-18 18:02:11 +0200

    Merged References class into Main Mod Class Changed GuiOpen event handler to lowest priority, hoping to fix compatibility with CustomMainMenu. Slightly changed current information text.

--- src/main/java/org/stopmodreposts/stopmodreposts/guis/InformationGui.java
+++ src/main/java/org/stopmodreposts/stopmodreposts/guis/InformationGui.java
@@ -3,7 +3,7 @@ package org.stopmodreposts.stopmodreposts.guis;
 import java.io.IOException;
 import java.util.List;
 
-import org.stopmodreposts.stopmodreposts.References;
+import org.stopmodreposts.stopmodreposts.StopModReposts;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
@@ -20,7 +20,7 @@ import net.minecraft.util.text.event.ClickEvent;
 
 public class InformationGui extends GuiScreen {
 
-	private static final ResourceLocation SMR_LOGO = new ResourceLocation(References.MODID, "textures/guis/smr_logo.png");
+	private static final ResourceLocation SMR_LOGO = new ResourceLocation(StopModReposts.MODID, "textures/guis/smr_logo.png");
 	private static final String TRANSLATION_BASE_KEY = "stopmodreposts.gui.information.";
 
 	private FontRenderer fontRenderer = null;
@@ -95,20 +95,20 @@ public class InformationGui extends GuiScreen {
 	@Override
     protected void actionPerformed(GuiButton button) throws IOException {
 		if(button == this.btnClose) {
-			System.out.println(this.parent);
 			Minecraft.getMinecraft().displayGuiScreen(this.parent);
 	    	try {
 	    		/*
 	    		 * TODO create a file containing some more information and maybe more links
 	    		 * Then copy it out of the assets
 	    		 */
-				References.STOP_REPOST_FILE.createNewFile();
+	    		StopModReposts.STOP_REPOST_FILE.createNewFile();
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}else if(button == this.btnMoreInfo) {
 			ITextComponent linkHelper = new TextComponentString("");
-			linkHelper.getStyle().setClickEvent(new ClickEvent( ClickEvent.Action.OPEN_URL, References.URL));
+			linkHelper.getStyle().setClickEvent(new ClickEvent( ClickEvent.Action.OPEN_URL, StopModReposts.URL));
 
 			this.handleComponentClick(linkHelper);
 		}
